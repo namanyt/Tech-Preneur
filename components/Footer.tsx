7725e9ef140a6918e73ab8dff94475fbe163c34c
Cider <cider@ciderboi.xyz>
2022-09-13 16:03:22 +0530

    Added my copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,74 +1,6 @@
-// import { createStyles, Container, Group, Anchor, Text } from '@mantine/core';
-// import { MantineLogo } from '@mantine/ds';
-// import Link from 'next/link';
-
-// const useStyles = createStyles((theme) => ({
-//   footer: {
-//     marginTop: 120,
-//     borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-//       }`,
-//   },
-
-//   inner: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     paddingTop: theme.spacing.xl,
-//     paddingBottom: theme.spacing.xl,
-
-//     [theme.fn.smallerThan('xs')]: {
-//       flexDirection: 'column',
-//     },
-//   },
-
-//   links: {
-//     [theme.fn.smallerThan('xs')]: {
-//       marginTop: theme.spacing.md,
-//     },
-//   },
-
-//   title: {
-//     fontSize: 'clamp(2vw, 1.5em, 2em)',
-//     fontWeight: 700,
-//     color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-//     textTransform: 'uppercase',
-//     lineHeight: 2,
-//     fontFamily: 'Montserrat',
-//   }
-
-// }));
-
-// interface FooterSimpleProps {
-//   links: { link: string; label: string }[];
-// }
-
-// export function FooterSimple({ links }: FooterSimpleProps) {
-//   const { classes } = useStyles();
-//   const items = links.map((link) => (
-//     <Link
-//       key={link.label}
-//       href={link.link}
-//       onClick={() => {
-//         window.scrollTo(0, 0);
-//       }}
-//     >
-//       <Anchor className={classes.links} color="dimmed">{link.label}</Anchor>
-//     </Link>
-//   ));
-
-//   return (
-//     <div className={classes.footer}>
-//       <Container className={classes.inner}>
-//         <Text className={classes.title}>Tech Preneur</Text>
-//         <Group className={classes.links}>{items}</Group>
-//       </Container>
-//     </div>
-//   );
-// }
-
-import { createStyles, Text, Container, ActionIcon, Group, Anchor } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons';
-import { MantineLogo } from '@mantine/ds';
+import {createStyles, Text, Container, ActionIcon, Group, Anchor} from '@mantine/core';
+import {IconBrandTwitter, IconBrandYoutube, IconBrandInstagram} from '@tabler/icons';
+import {MantineLogo} from '@mantine/ds';
 
 const useStyles = createStyles((theme) => ({
   footer: {
@@ -175,14 +107,14 @@ const useStyles = createStyles((theme) => ({
 interface FooterLinksProps {
   data: {
     title: string;
-    links?: { label: string; link: string }[];
+    links?: {label: string; link: string}[];
     descirption?: string[];
   }[];
   style?: any;
 }
 
-export function FooterSimple({ data, style }: FooterLinksProps) {
-  const { classes } = useStyles();
+export function FooterSimple({data, style}: FooterLinksProps) {
+  const {classes} = useStyles();
 
   const groups = data.map((group) => {
     const links = group.links?.map((link, index) => (
@@ -219,14 +151,15 @@ export function FooterSimple({ data, style }: FooterLinksProps) {
         <div className={classes.logo}>
           <Text className={classes.title}>Tech Preneur</Text>
           <Text size="xs" color="dimmed" className={classes.description}>
-            <Anchor href='https://joyfulmerger.tk' variant='text'>https://joyfulmerger.tk</Anchor>
+            <Anchor href='https://joyfulmerger.tk' variant='link' color='dimmed'>https://joyfulmerger.tk</Anchor>
           </Text>
         </div>
         <div className={classes.groups}>{groups}</div>
       </Container>
       <Container className={classes.afterFooter}>
         <Text color="dimmed" size="sm">
-          © 2022 All rights reserved with DPS Jaipur
+          © 2022 All rights reserved with <Anchor href='https://dpsjaipur.com' variant='link' color='dimmed'>DPS Jaipur</Anchor> <br />
+          © 2022 All rights reserved with <Anchor href='https://ciderboi.xyz' variant='link' color='dimmed'>Cider Boi</Anchor>
         </Text>
 
         <Group spacing={0} className={classes.social} position="right" noWrap>
